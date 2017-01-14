94800d70983e8ddab801c789c3174738cbe9b8a2
Jefferson Borges <jeffersonbfs@gmail.com>
2017-01-14 18:06:42 -0200

    realizando a consulta de produtos

--- node/app/routes/produtos.js
+++ node/app/routes/produtos.js
@@ -1,5 +1,16 @@
 module.exports = function(app) {
   app.get('/produtos', function(req, res){
-    res.render("produtos/lista");
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+      host : 'localhost',
+      user : 'root',
+      password : 'admin',
+      database : 'curso_nodejs'
+    });
+
+    connection.query('select * from livros', function(err, results) {
+      res.send(results);
+    });
+    connection.end();
   });
 };
